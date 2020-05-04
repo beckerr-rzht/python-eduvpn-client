b841338fd07de2c5ff6f7ead554075014054e21e
Gijs Molenaar <gijs@pythonic.nl>
2020-05-04 17:33:25 +0200

    demo flow automated

--- cleanup.py
+++ cleanup.py
@@ -1,14 +1,16 @@
 import sys
-import requests
+from tempfile import mkdtemp
+from pathlib import Path
+from shutil import rmtree
 from typing import Dict, Union
 from itertools import chain
 import webbrowser
+import requests
 from requests_oauthlib import OAuth2Session
-# import gi
-# import tempfile
+import gi
 
-# gi.require_version('NM', '1.0')
-# from gi.repository import GLib, NM
+gi.require_version('NM', '1.0')
+from gi.repository import GLib, NM
 from eduvpn.oauth2 import one_request, get_open_port
 from eduvpn.crypto import gen_code_challenge, gen_code_verifier, common_name_from_cert
 
@@ -145,12 +147,54 @@ def menu() -> str:
             sys.exit(1)
 
 
+def write_config(config: str):
+    with open('eduvpn.ovpn', mode='w+t') as f:
+        f.writelines(config)
+
+
+def ovpn_import(target: Path):
+    for vpn_info in NM.VpnPluginInfo.list_load():
+        try:
+            return vpn_info.load_editor_plugin().import_(str(target))
+        except Exception as e:
+            print(f"can't import config: {e}")
+
+
+def import_config(config: str, private_key: str, certificate: str):
+    target_parent = Path(mkdtemp())
+    target = target_parent / "eduVPN.ovpn"
+
+    with open(target, mode='w+t') as f:
+        f.writelines(config)
+        f.writelines(f"\n<key>\n{private_key}\n</key>\n")
+        f.writelines(f"\n<cert>\n{certificate}\n</cert>\n")
+
+    connection = ovpn_import(target)
+    connection.normalize()
+    client = NM.Client.new(None)
+    main_loop = GLib.MainLoop()
+
+    def added_cb(client, result, _):
+        try:
+            client.add_connection_finish(result)
+            print("The connection profile has been successfully added to NetworkManager.")
+        except Exception as e:
+            print("ERROR: failed to add connection: %s\n" % e)
+        main_loop.quit()
+
+    client.add_connection_async(connection, True, None, added_cb, None)
+    main_loop.run()
+    rmtree(target_parent)
+
+
 def main():
     url = menu()
     api_base_uri, token_endpoint, auth_endpoint = get_info(url)
     oauth = get_oauth(token_endpoint, auth_endpoint)
     profile_id = list_profiles(oauth, api_base_uri)[0]['profile_id']
-    print(get_config(oauth, api_base_uri, profile_id))
+    config = get_config(oauth, api_base_uri, profile_id)
+    private_key, certificate = create_keypair(oauth, api_base_uri)
+    import_config(config, private_key, certificate)
 
 
 if __name__ == '__main__':
